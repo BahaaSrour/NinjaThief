1b829d7dfc253cd1c8a044e749d808888849d808
BahaaSrour <dev.bahaasrour@gmail.com>
2020-01-01 18:22:28 +0200

    added character details

--- Character.js
+++ Character.js
@@ -1,18 +1,34 @@
 console.log("Character file are oped")
 
-function Character(name, age, health, lives,startPosition) {
-    this.name =name
-    this.age = age;
-    this.health = health;
-    this.lives = lives;
-    this.startPosition=startPosition;
+function Character(_name, _health, _lives, _startPositionX,_startPositionY, _positionX,_positionY, _Noshuriken, _AttackRang, _direction) {
+    this.name =_name
+    this.health = _health;
+    this.lives = _lives;
+    this.startPositionX=_startPositionX;
+    this.startPositiony=_startPositionY;
+    this.Position = _positionX;
+    this.Position = _positionY;
+    this.Noshuriken = _Noshuriken;
+    this.AttackRang = _AttackRang
+    this.direction = _direction
   };
 
   Character.prototype.walk = function() {
+    this.Position +=10;
     console.log("Walk Function from character class");
   };
-
-  Character.prototype.Attack = function() {
-    console.log("Walk Function from character class");
+  
+  Character.prototype.Attcked = function(dropPionts) {
+    this.health -=dropPionts;
+    console.log("dropPionts Function from character class");
   };
 
+  Character.prototype.ChangeDirection =function () {
+    // if(this.)
+  }
+
+  Character.prototype.GetData =function () {
+    return this.name;
+  }
+ 
+  
--- Manger.js
+++ Manger.js
@@ -1,38 +1,33 @@
-{/* <meta charset="utf-8"/> */}
-console.log("Manager file are Opened OF Ninja Game")
+
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-
+    width: 1200,
+    height: 700,
     physics: {
         default: 'arcade',
         arcade: {
             gravity: { y: 200 }
-            , debug:true
         }
     },
-
     scene: {
         preload: preload,
-        create: create,
-        update: update
+        create: create
     }
 };
 
+var game = new Phaser.Game(config);
+
 function preload ()
 {
-        console.log("preload Function are oped")
-        //this.load.image("sky" , "Assets/sky.png");
+    //character and trying creating an object
+//    var person= new Character("ali",100,3,50,50,80,80,3,15,1)
+//    console.log("name: "+ person.GetData())
+
+
+    this.load.image('red', 'Assets/sky.png')
 }
-    console.log(this)
-preload();
-    function create ()
-    {
-        console.log("create Function are oped")
-    }
-    
-    function update ()
-    {
-        console.log("update file are oped")
-    }
+
+function create ()
+{
+ this.add.image(100,100,"red").setScale(2);
+}
