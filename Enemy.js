cbb811e14c6bed47a8798bab833c6aa613d8b5c8
Mohamed Karam <mohamedkaram09@gmail.com>
2020-01-01 08:59:50 +0200

    Initialization enemy

---
+++ Enemy.js
@@ -0,0 +1,22 @@
+var Enemy = {
+     health: 0,
+     positionRange: new Array(4),
+     visualRange: 20,
+     attackRnage: 15,
+
+    Attack: function () {
+        //throw weapon animation
+    },
+
+    Walk: function (speed) {
+        //setpositionX
+        //walk animation at 'speed'  
+    },
+
+    jump: function () {
+        //setpostitonY
+    },
+
+      
+    
+};
